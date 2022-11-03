fb16689f6954819850e273e38814fd14398f2971
dnwndalsrl <dnwndwlsqja@gmail.com>
2022-11-03 17:52:04 +0900

    add

---
+++ 대문자와 소문자.js
@@ -0,0 +1,16 @@
+// 문자열 my_string이 매개변수로 주어질 때, 대문자는 소문자로 소문자는 대문자로 변환한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 1 ≤ my_string의 길이 ≤ 1,000
+// my_string은 영어 대문자와 소문자로만 구성되어 있습니다.
+
+function solution(my_string) {
+    let result = ''
+    for(let i = 0; i < my_string.length; i++) {
+        if(my_string[i] === my_string[i].toUpperCase()) {
+            result += my_string[i].toLowerCase()
+        } else {
+            result += my_string[i].toUpperCase()
+        }
+    }
+    return result
+}
